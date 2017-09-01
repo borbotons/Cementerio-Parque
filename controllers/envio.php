eb7b0e004ec74a25a2b62acc2632b1670b33cafe
danielthewolf93 <danieltesorero@gmail.com>
2017-09-01 00:48:34 -0300

    Correc_envio

--- controllers/envio.php
+++ controllers/envio.php
@@ -5,29 +5,13 @@
             //Paso de datos mediante la url.
             $nombre = $_POST['name'];
             $mail = $_POST['email'];
-            $domicilio = $_POST['autocomplete'];
+            $domicilio = $_POST['domicilio'];
             $telefono = $_POST['tel'];
-            $asuna = $_POST['some_field'];
-
-            $num = count($asuna);
-
-            echo 'Checkbox seleccionado'.'su valor es:'.$num;
-
-
-
-          /*  echo $asuna[];
-            if($asuna.[$n]==0 )
-            {
-                $asun='Seguro Sepelio';
-            
-            }*/
-
-            //if($asun)
-
-
-
-/*
+            $asun = $_POST['some_field'];
             $mensajeC = $_POST['message'];
+
+
+           
             //Cabecera del correo
             $header = 'From: ' . $mail . " \r\n";
             $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -47,17 +31,19 @@
             //Mensajes de Exito o Fallo
             if (mail($para, $asunto, utf8_decode($mensaje), $header)) {
                 
-               header( "refresh:0;../contacto.html" );
+               header( "refresh:0;http://www.parquevirgen.com.mialias.net/contacto.html" );
                echo "<script>alert('Mensaje enviado, ¡Gracias!.');</script>";
                die();
+               //die("Gracias, su mensaje se envio correctamente.");
               
-               $mensajeExito = '<div class="alert alert-success" role="alert">Mensaje enviado.A la brevedad nos comunicaremos contigo.Gracias.</div>';  
+              // $mensajeExito = '<div class="alert alert-success" role="alert">Mensaje enviado.A la brevedad nos comunicaremos contigo.Gracias.</div>';  
                 //Ver para no salir de la pagina y seguir en la misma ademas creo q tendre q validar aca tambien, porque sino no vale.
                 //el jquery q mande.
-                //Tomar bien la opcion del asun.(el de textarea) Ver el tema si selecciono if 1=Sepelio 2=Crematorio 3=Parcela
+
             } else {
-                echo "<script>alert('Error no se envio el mensaje,por favor intente mas tarde ¡Gracias!.');</script>";
+
+               //echo  $error = '<div class="alert alert-danger" role="alert"><p><strong>Mensaje sin enviar.Prueba nuevamente,disculpa las molestias.</div>';  
             } 
           
-  */  
+    
 ?>
